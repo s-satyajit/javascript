b9156fe0eaf3631eb815ff2c2eed57fc650d0587
Satyajit Samal <samalsatyajit668@gmail.com>
2024-05-12 16:59:00 +0530

    Added new file

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,4 +23,32 @@
 let someNumber = 33
 let stringNumber = String(someNumber)
 
-console.log(stringNumber)
+// console.log(stringNumber)
+
+
+
+// ****************************       Operations     **************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+let str1 = "hello"
+let str2 = "satyajit"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(true);
+
+let gameCounter = 100
+gameCounter++;
+// ++gameCounter;
+console.log(gameCounter);
