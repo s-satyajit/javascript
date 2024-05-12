21a2baae1907a1211ac06cd45566ba009ddcf7ca
Satyajit Samal <samalsatyajit668@gmail.com>
2024-05-12 12:48:32 +0530

    Added some new files

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 133553
+let accountEmail = "satyajit.speaks@gmail.com"
+var accountPassword = "12345"
+accountCity = "Bhubaneswar"
+let accountState;
+
+// accountId = 2   // not allowed
+
+accountEmail = "ssamal.com"
+accountPassword = "23434"
+accountCity = "Bangalore"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"  // treat all JS code as newer version
+
+//alert(3 + 3)   we are using node, not browser
+
+let name = "Satyajit"
+let age = 22
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+// console.log(typeof "Satyajit")
+// console.log(typeof undefined);
+console.log(typeof null);
