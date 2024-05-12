497bde66024b5e354992727525853f624fc25a21
Satyajit Samal <samalsatyajit668@gmail.com>
2024-05-12 17:32:57 +0530

    Added new file

---
+++ datatypes-summary.js
@@ -0,0 +1,37 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+const score = 100
+const scoreValue  = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;;;;;;;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 3453645697456743745345n;
+
+// Reference (Non primitive)
+
+//Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+
+let myObj = {
+    name: "satyajit",
+    age: 22,
+}
+
+const myFunction = function() {
+    console.log("Hello World");
+}
+
+// console.log(typeof bigNumber)
+console.log(typeof myFunction)
+
