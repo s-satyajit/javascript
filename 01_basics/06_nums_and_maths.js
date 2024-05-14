be5ef5a3f6efcfaaffa9d2e7aaab98f3497d2dee
Satyajit Samal <samalsatyajit668@gmail.com>
2024-05-14 08:00:50 +0530

    Added a new file

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,28 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.9454
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++++++  Maths +++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 23, 43, 2, 78));
+// console.log(Math.max(34, 463, 234, 65));
+
+// console.log((Math.random()*10)+ 1);
+
+console.log(Math.floor(Math.random() * (7-1)) + 1);
