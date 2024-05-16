d20733ab1a1060a88384a8a07d62d175d2aff4ad
Satyajit Samal <samalsatyajit668@gmail.com>
2024-05-16 11:45:04 +0530

    Added a new file

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,55 @@
+const tinder = new Object()
+
+const tinderUser = {}
+
+// console.log(tinder)
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser)
+
+const regularUser = {
+    email: "samalsatyajit668@gmail.com",
+    fullname: {
+        firstname: "Satyajit",
+        lastname: "Samal"
+    },
+    age: 22
+}
+
+// console.log(regularUser)
+// console.log(regularUser.fullname.lastname)
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = Object.assign(obj1, obj2)
+// const obj3 = Object.assign({}, obj1, obj2)
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3)
+
+const users = [
+    {
+        id: 1,
+        email: "s@gmail.com"
+    },
+    {
+        id: 2,
+        email: "ssamal@gmail.com"
+    },
+    {
+        id: 3,
+        email: "satyajit@gmail.com"
+    }
+]
+
+// console.log(users[0].id)
+// console.log(users[0].email)
+
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
+
+console.log(tinderUser.hasOwnProperty("name"))
