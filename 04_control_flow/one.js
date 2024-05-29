7e0e2c6285163ed36dea78ebb2b6682fa52c9f9b
Satyajit Samal <samalsatyajit668@gmail.com>
2024-05-29 13:02:22 +0530

    Added two new files

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -41,3 +41,22 @@ if(loggedInFromGoogle || loggedInFromEmail) {
     console.log("User logged in");
 }
 
+
+//Nullish Coalescing Operator (??): null undefined
+
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+console.log(val1);
+
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice >= 80 ? console.log("Less than 80") : console.log("More than 80")
